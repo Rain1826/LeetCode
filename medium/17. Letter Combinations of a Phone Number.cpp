3e4c1e9e63bed6a4d46e89f14756c0434018f5c0
Rain <lca1826@163.com>
2017-06-07 11:24:47 +0800

    Create 17. Letter Combinations of a Phone Number.cpp

---
+++ medium/17. Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,31 @@
+/*Given a digit string, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below.(计算器)
+
+Input:Digit string "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+Note:
+Although the above answer is in lexicographical order, your answer could be in any order you want.*/
+
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if(digits.length() == 0){
+            return vector<string>();
+        }
+        string str[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> ans;
+        ans.push_back("");
+        for(int i = 0;i < digits.size(); ++ i) {
+            string tempstr = str[digits[i] - '0'];
+            vector<string> tempans;
+            for(int j = 0;j < tempstr.size(); ++ j) {
+                for(int k = 0;k < ans.size(); ++ k) {
+                    tempans.push_back(ans[k] + tempstr[j]);
+                }
+            }
+            ans = tempans;
+        }
+        return ans;
+    }
+};
