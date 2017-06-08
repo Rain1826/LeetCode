3f1b0d0fd6d3f4013aa1d6ead49633ea651065e5
Rain <lca1826@163.com>
2017-06-08 16:57:07 +0800

    Update 29. Divide Two Integers.cpp

--- medium/29. Divide Two Integers.cpp
+++ medium/29. Divide Two Integers.cpp
@@ -7,31 +7,31 @@ If it is overflow, return MAX_INT.*/
 位运算实现除法
 */
 
-class Solution {
+class Solution
+{
 public:
-    int divide(int dividend, int divisor) {
-        int ans = 0;
+    int divide(int dividend, int divisor)
+    {
         const int MAX_INT = 2147483647;
         const int MIN_INT = -2147483648;
-        if(divisor == 0)    return MAX_INT;
-        if(divisor == 1)    return dividend;
-        if(divisor == -1) {
-            if(dividend == MIN_INT) return MAX_INT;
-            else    return -dividend;
-        }
-        bool flag = ((dividend < 0) ^ (divisor < 0)) ? false : true;
+        if (!divisor || (dividend == MIN_INT && divisor == -1))
+            return MAX_INT;
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         long long dvd = abs((long long)dividend);
         long long dvs = abs((long long)divisor);
-        
-        while (dvd >= dvs) { 
+        int res = 0;
+        while (dvd >= dvs)
+        {
             long long temp = dvs, multiple = 1;
-            while (dvd >= (temp << 1)) {
+            while (dvd >= (temp << 1))
+            {
                 temp <<= 1;
                 multiple <<= 1;
             }
             dvd -= temp;
-            ans += multiple;
+            res += multiple;
         }
-        return flag ? ans : -ans;
+        return sign == 1 ? res : -res;
     }
 };
+
