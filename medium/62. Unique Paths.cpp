b3f6557523e5ce6dff409b2126cd2c34d26515bd
Brain <lca1826@163.com>
2017-06-19 19:56:07 +0800

    Create 62. Unique Paths.cpp

---
+++ medium/62. Unique Paths.cpp
@@ -0,0 +1,34 @@
+/*A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.*/
+
+/*
+思路：
+可以用排列组合知识知道最终结果为 (m + n)! / (m! * n!)
+但直接把每个阶乘求除法的话会溢出，因此将该过程转化为分子和分母分别作乘法和除法
+*/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if(m == 1 || n == 1) {
+            return 1;
+        }
+        if(m < n) {
+            swap(m, n);
+        }
+        int N = m + n - 2;
+        long long res = 1;
+        for(int i = m, j = 1;i <= N;i ++, j ++) {
+            res *= i;
+            res /= j;
+        }
+        return (int)res;
+    }
+};
