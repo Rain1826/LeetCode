3e0e849695aa600c325b19b354e063fa3897e811
Brain <lca1826@163.com>
2017-06-19 20:27:19 +0800

    Update 62. Unique Paths.cpp

--- medium/62. Unique Paths.cpp
+++ medium/62. Unique Paths.cpp
@@ -10,10 +10,14 @@ Note: m and n will be at most 100.*/
 
 /*
 思路：
-可以用排列组合知识知道最终结果为 (m + n)! / (m! * n!)
-但直接把每个阶乘求除法的话会溢出，因此将该过程转化为分子和分母分别作乘法和除法
+1.可以用排列组合知识知道最终结果为 (m + n)! / (m! * n!)
+  但直接把每个阶乘求除法的话会溢出，因此将该过程转化为分子和分母分别作乘法和除法
+2.动态规划
+  对于格点(i,j)。由于只能从上格点(i-1,j)或左格点(i,j-1)到达，并且两者路径是不重复的
+  因此dp[i][j] = dp[i-1][j]+dp[i][j-1]
 */
 
+//排列组合
 class Solution {
 public:
     int uniquePaths(int m, int n) {
@@ -30,5 +34,19 @@ public:
             res /= j;
         }
         return (int)res;
+    }
+};
+
+//动态规划
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 1));
+        for(int i = 1;i < m;i ++) {
+            for(int j = 1;j < n;j ++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 };
